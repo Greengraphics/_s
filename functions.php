1cad7ffd11cc157636c149ecc772bad9ad3761d2
Nathan Gearing <natgearing@gmail.com>
2017-09-22 16:28:34 +1000

    More fix ups and adding phpcs ruleset

--- functions.php
+++ functions.php
@@ -14,17 +14,18 @@
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function ggstyle_setup() {
+function ggstyle_setup()
+{
     /*
      * Make theme available for translation.
      * Translations can be filed in the /languages/ directory.
      * If you're building a theme based on _s, use a find and replace
      * to change 'ggstyle' to the name of your theme in all the template files.
      */
-    load_theme_textdomain( 'ggstyle', get_template_directory() . '/languages' );
+    load_theme_textdomain('ggstyle', get_template_directory() . '/languages');
 
     // Add default posts and comments RSS feed links to head.
-    add_theme_support( 'automatic-feed-links' );
+    add_theme_support('automatic-feed-links');
 
     /*
      * Let WordPress manage the document title.
@@ -32,54 +33,54 @@ function ggstyle_setup() {
      * hard-coded <title> tag in the document head, and expect WordPress to
      * provide it for us.
      */
-    add_theme_support( 'title-tag' );
+    add_theme_support('title-tag');
 
     /*
      * Enable support for Post Thumbnails on posts and pages.
      *
      * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
      */
-    add_theme_support( 'post-thumbnails' );
+    add_theme_support('post-thumbnails');
 
     // This theme uses wp_nav_menu() in one location.
-    register_nav_menus( array(
-        'menu-1' => esc_html__( 'Primary', 'ggstyle' ),
-    ) );
+    register_nav_menus(array(
+        'menu-1' => esc_html__('Primary', 'ggstyle'),
+    ));
 
     /*
      * Switch default core markup for search form, comment form, and comments
      * to output valid HTML5.
      */
-    add_theme_support( 'html5', array(
+    add_theme_support('html5', array(
         'search-form',
         'comment-form',
         'comment-list',
         'gallery',
         'caption',
-    ) );
+    ));
 
     // Set up the WordPress core custom background feature.
-    add_theme_support( 'custom-background', apply_filters( 'ggstyle_custom_background_args', array(
+    add_theme_support('custom-background', apply_filters('ggstyle_custom_background_args', array(
         'default-color' => 'ffffff',
         'default-image' => '',
-    ) ) );
+    )));
 
     // Add theme support for selective refresh for widgets.
-    add_theme_support( 'customize-selective-refresh-widgets' );
+    add_theme_support('customize-selective-refresh-widgets');
 
     /**
      * Add support for core custom logo.
      *
      * @link https://codex.wordpress.org/Theme_Logo
      */
-    add_theme_support( 'custom-logo', array(
+    add_theme_support('custom-logo', array(
         'height'      => 250,
         'width'       => 250,
         'flex-width'  => true,
         'flex-height' => true,
-    ) );
+    ));
 }
-add_action( 'after_setup_theme', 'ggstyle_setup' );
+add_action('after_setup_theme', 'ggstyle_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -88,44 +89,47 @@ add_action( 'after_setup_theme', 'ggstyle_setup' );
  *
  * @global int $content_width
  */
-function ggstyle_content_width() {
-    $GLOBALS['content_width'] = apply_filters( 'ggstyle_content_width', 640 );
+function ggstyle_content_width()
+{
+    $GLOBALS['content_width'] = apply_filters('ggstyle_content_width', 640);
 }
-add_action( 'after_setup_theme', 'ggstyle_content_width', 0 );
+add_action('after_setup_theme', 'ggstyle_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function ggstyle_widgets_init() {
-    register_sidebar( array(
-        'name'          => esc_html__( 'Sidebar', 'ggstyle' ),
+function ggstyle_widgets_init()
+{
+    register_sidebar(array(
+        'name'          => esc_html__('Sidebar', 'ggstyle'),
         'id'            => 'sidebar-1',
-        'description'   => esc_html__( 'Add widgets here.', 'ggstyle' ),
+        'description'   => esc_html__('Add widgets here.', 'ggstyle'),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
         'before_title'  => '<h2 class="widget-title">',
         'after_title'   => '</h2>',
-    ) );
+    ));
 }
-add_action( 'widgets_init', 'ggstyle_widgets_init' );
+add_action('widgets_init', 'ggstyle_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function ggstyle_scripts() {
-    wp_enqueue_style( 'ggstyle-style', get_stylesheet_uri() );
+function ggstyle_scripts()
+{
+    wp_enqueue_style('ggstyle-style', get_stylesheet_uri());
 
-    wp_enqueue_script( 'ggstyle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    wp_enqueue_script('ggstyle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true);
 
-    wp_enqueue_script( 'ggstyle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script('ggstyle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
     }
 }
-add_action( 'wp_enqueue_scripts', 'ggstyle_scripts' );
+add_action('wp_enqueue_scripts', 'ggstyle_scripts');
 
 /**
  * Implement the Custom Header feature.
--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -15,24 +15,26 @@
  *
  * @return void
  */
-function _s_woocommerce_setup() {
-	add_theme_support( 'woocommerce' );
-	add_theme_support( 'wc-product-gallery-zoom' );
-	add_theme_support( 'wc-product-gallery-lightbox' );
-	add_theme_support( 'wc-product-gallery-slider' );
+function _s_woocommerce_setup()
+{
+    add_theme_support('woocommerce');
+    add_theme_support('wc-product-gallery-zoom');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
 }
-add_action( 'after_setup_theme', '_s_woocommerce_setup' );
+add_action('after_setup_theme', '_s_woocommerce_setup');
 
 /**
  * WooCommerce specific scripts & stylesheets.
  *
  * @return void
  */
-function _s_woocommerce_scripts() {
-	wp_enqueue_style( '_s-woocommerce-style', get_template_directory_uri() . '/woocommerce.css' );
+function _s_woocommerce_scripts()
+{
+    wp_enqueue_style('_s-woocommerce-style', get_template_directory_uri() . '/woocommerce.css');
 
-	$font_path   = WC()->plugin_url() . '/assets/fonts/';
-	$inline_font = '@font-face {
+    $font_path   = WC()->plugin_url() . '/assets/fonts/';
+    $inline_font = '@font-face {
 			font-family: "star";
 			src: url("' . $font_path . 'star.eot");
 			src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
@@ -43,9 +45,9 @@ function _s_woocommerce_scripts() {
 			font-style: normal;
 		}';
 
-	wp_add_inline_style( '_s-woocommerce-style', $inline_font );
+    wp_add_inline_style('_s-woocommerce-style', $inline_font);
 }
-add_action( 'wp_enqueue_scripts', '_s_woocommerce_scripts' );
+add_action('wp_enqueue_scripts', '_s_woocommerce_scripts');
 
 /**
  * Disable the default WooCommerce stylesheet.
@@ -55,7 +57,7 @@ add_action( 'wp_enqueue_scripts', '_s_woocommerce_scripts' );
  *
  * @link https://docs.woocommerce.com/document/disable-the-default-stylesheet/
  */
-add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+add_filter('woocommerce_enqueue_styles', '__return_empty_array');
 
 /**
  * Add 'woocommerce-active' class to the body tag.
@@ -63,42 +65,46 @@ add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
  * @param  array $classes CSS classes applied to the body tag.
  * @return array $classes modified to include 'woocommerce-active' class.
  */
-function _s_woocommerce_active_body_class( $classes ) {
-	$classes[] = 'woocommerce-active';
+function _s_woocommerce_active_body_class($classes)
+{
+    $classes[] = 'woocommerce-active';
 
-	return $classes;
+    return $classes;
 }
-add_filter( 'body_class', '_s_woocommerce_active_body_class' );
+add_filter('body_class', '_s_woocommerce_active_body_class');
 
 /**
  * Products per page.
  *
  * @return integer number of products.
  */
-function _s_woocommerce_products_per_page() {
-	return 12;
+function _s_woocommerce_products_per_page()
+{
+    return 12;
 }
-add_filter( 'loop_shop_per_page', '_s_woocommerce_products_per_page' );
+add_filter('loop_shop_per_page', '_s_woocommerce_products_per_page');
 
 /**
  * Product gallery thumnbail columns.
  *
  * @return integer number of columns.
  */
-function _s_woocommerce_thumbnail_columns() {
-	return 4;
+function _s_woocommerce_thumbnail_columns()
+{
+    return 4;
 }
-add_filter( 'woocommerce_product_thumbnails_columns', '_s_woocommerce_thumbnail_columns' );
+add_filter('woocommerce_product_thumbnails_columns', '_s_woocommerce_thumbnail_columns');
 
 /**
  * Default loop columns on product archives.
  *
  * @return integer products per row.
  */
-function _s_woocommerce_loop_columns() {
-	return 3;
+function _s_woocommerce_loop_columns()
+{
+    return 3;
 }
-add_filter( 'loop_shop_columns', '_s_woocommerce_loop_columns' );
+add_filter('loop_shop_columns', '_s_woocommerce_loop_columns');
 
 /**
  * Related Products Args.
@@ -106,159 +112,165 @@ add_filter( 'loop_shop_columns', '_s_woocommerce_loop_columns' );
  * @param array $args related products args.
  * @return array $args related products args.
  */
-function _s_woocommerce_related_products_args( $args ) {
-	$defaults = array(
-		'posts_per_page' => 3,
-		'columns'        => 3,
-	);
+function _s_woocommerce_related_products_args($args)
+{
+    $defaults = array(
+        'posts_per_page' => 3,
+        'columns'        => 3,
+    );
 
-	$args = wp_parse_args( $defaults, $args );
+    $args = wp_parse_args($defaults, $args);
 
-	return $args;
+    return $args;
 }
-add_filter( 'woocommerce_output_related_products_args', '_s_woocommerce_related_products_args' );
+add_filter('woocommerce_output_related_products_args', '_s_woocommerce_related_products_args');
 
-if ( ! function_exists( '_s_woocommerce_product_columns_wrapper' ) ) {
-	/**
-	 * Product columns wrapper.
-	 *
-	 * @return  void
-	 */
-	function _s_woocommerce_product_columns_wrapper() {
-		$columns = _s_woocommerce_loop_columns();
-		echo '<div class="columns-' . absint( $columns ) . '">';
-	}
+if (! function_exists('_s_woocommerce_product_columns_wrapper')) {
+    /**
+     * Product columns wrapper.
+     *
+     * @return  void
+     */
+    function _s_woocommerce_product_columns_wrapper()
+    {
+        $columns = _s_woocommerce_loop_columns();
+        echo '<div class="columns-' . absint($columns) . '">';
+    }
 }
-add_action( 'woocommerce_before_shop_loop', '_s_woocommerce_product_columns_wrapper', 40 );
+add_action('woocommerce_before_shop_loop', '_s_woocommerce_product_columns_wrapper', 40);
 
-if ( ! function_exists( '_s_woocommerce_product_columns_wrapper_close' ) ) {
-	/**
-	 * Product columns wrapper close.
-	 *
-	 * @return  void
-	 */
-	function _s_woocommerce_product_columns_wrapper_close() {
-		echo '</div>';
-	}
+if (! function_exists('_s_woocommerce_product_columns_wrapper_close')) {
+    /**
+     * Product columns wrapper close.
+     *
+     * @return  void
+     */
+    function _s_woocommerce_product_columns_wrapper_close()
+    {
+        echo '</div>';
+    }
 }
-add_action( 'woocommerce_after_shop_loop', '_s_woocommerce_product_columns_wrapper_close', 40 );
+add_action('woocommerce_after_shop_loop', '_s_woocommerce_product_columns_wrapper_close', 40);
 
 /**
  * Remove default WooCommerce wrapper.
  */
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
-if ( ! function_exists( '_s_woocommerce_wrapper_before' ) ) {
-	/**
-	 * Before Content.
-	 *
-	 * Wraps all WooCommerce content in wrappers which match the theme markup.
-	 *
-	 * @return void
-	 */
-	function _s_woocommerce_wrapper_before() {
-		?>
+if (! function_exists('_s_woocommerce_wrapper_before')) {
+    /**
+     * Before Content.
+     *
+     * Wraps all WooCommerce content in wrappers which match the theme markup.
+     *
+     * @return void
+     */
+    function _s_woocommerce_wrapper_before()
+    {
+        ?>
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
 		<?php
-	}
+    }
 }
-add_action( 'woocommerce_before_main_content', '_s_woocommerce_wrapper_before' );
+add_action('woocommerce_before_main_content', '_s_woocommerce_wrapper_before');
 
-if ( ! function_exists( '_s_woocommerce_wrapper_after' ) ) {
-	/**
-	 * After Content.
-	 *
-	 * Closes the wrapping divs.
-	 *
-	 * @return void
-	 */
-	function _s_woocommerce_wrapper_after() {
-		?>
+if (! function_exists('_s_woocommerce_wrapper_after')) {
+    /**
+     * After Content.
+     *
+     * Closes the wrapping divs.
+     *
+     * @return void
+     */
+    function _s_woocommerce_wrapper_after()
+    {
+        ?>
 			</main><!-- #main -->
 		</div><!-- #primary -->
 		<?php
-	}
+    }
 }
-add_action( 'woocommerce_after_main_content', '_s_woocommerce_wrapper_after' );
+add_action('woocommerce_after_main_content', '_s_woocommerce_wrapper_after');
 
 /**
  * Sample implementation of the WooCommerce Mini Cart.
  *
  * You can add the WooCommerce Mini Cart to header.php like so ...
  *
-	<?php
-		if ( function_exists( '_s_woocommerce_header_cart' ) ) {
-			_s_woocommerce_header_cart();
-		}
-	?>
+    <?php
+        if ( function_exists( '_s_woocommerce_header_cart' ) ) {
+            _s_woocommerce_header_cart();
+        }
+    ?>
  */
 
-if ( ! function_exists( '_s_woocommerce_cart_link_fragment' ) ) {
-	/**
-	 * Cart Fragments.
-	 *
-	 * Ensure cart contents update when products are added to the cart via AJAX.
-	 *
-	 * @param array $fragments Fragments to refresh via AJAX.
-	 * @return array Fragments to refresh via AJAX.
-	 */
-	function _s_woocommerce_cart_link_fragment( $fragments ) {
-		ob_start();
-		_s_woocommerce_cart_link();
-		$fragments['a.cart-contents'] = ob_get_clean();
+if (! function_exists('_s_woocommerce_cart_link_fragment')) {
+    /**
+     * Cart Fragments.
+     *
+     * Ensure cart contents update when products are added to the cart via AJAX.
+     *
+     * @param array $fragments Fragments to refresh via AJAX.
+     * @return array Fragments to refresh via AJAX.
+     */
+    function _s_woocommerce_cart_link_fragment($fragments)
+    {
+        ob_start();
+        _s_woocommerce_cart_link();
+        $fragments['a.cart-contents'] = ob_get_clean();
 
-		return $fragments;
-	}
+        return $fragments;
+    }
 }
-add_filter( 'woocommerce_add_to_cart_fragments', '_s_woocommerce_cart_link_fragment' );
+add_filter('woocommerce_add_to_cart_fragments', '_s_woocommerce_cart_link_fragment');
 
-if ( ! function_exists( '_s_woocommerce_cart_link' ) ) {
-	/**
-	 * Cart Link.
-	 *
-	 * Displayed a link to the cart including the number of items present and the cart total.
-	 *
-	 * @return void
-	 */
-	function _s_woocommerce_cart_link() {
-		?>
-			<a class="cart-contents" href="<?php echo esc_url( wc_get_cart_url() ); ?>" title="<?php esc_attr_e( 'View your shopping cart', '_s' ); ?>">
+if (! function_exists('_s_woocommerce_cart_link')) {
+    /**
+     * Cart Link.
+     *
+     * Displayed a link to the cart including the number of items present and the cart total.
+     *
+     * @return void
+     */
+    function _s_woocommerce_cart_link()
+    {
+        ?>
+			<a class="cart-contents" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php esc_attr_e('View your shopping cart', '_s'); ?>">
 				<?php /* translators: number of items in the mini cart. */ ?>
-				<span class="amount"><?php echo wp_kses_data( WC()->cart->get_cart_subtotal() ); ?></span> <span class="count"><?php echo wp_kses_data( sprintf( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count(), '_s' ), WC()->cart->get_cart_contents_count() ) );?></span>
+				<span class="amount"><?php echo wp_kses_data(WC()->cart->get_cart_subtotal()); ?></span> <span class="count"><?php echo wp_kses_data(sprintf(_n('%d item', '%d items', WC()->cart->get_cart_contents_count(), '_s'), WC()->cart->get_cart_contents_count())); ?></span>
 			</a>
 		<?php
-	}
+    }
 }
 
-if ( ! function_exists( '_s_woocommerce_header_cart' ) ) {
-	/**
-	 * Display Header Cart.
-	 *
-	 * @return void
-	 */
-	function _s_woocommerce_header_cart() {
-		if ( is_cart() ) {
-			$class = 'current-menu-item';
-		} else {
-			$class = '';
-		}
-		?>
+if (! function_exists('_s_woocommerce_header_cart')) {
+    /**
+     * Display Header Cart.
+     *
+     * @return void
+     */
+    function _s_woocommerce_header_cart()
+    {
+        if (is_cart()) {
+            $class = 'current-menu-item';
+        } else {
+            $class = '';
+        } ?>
 		<ul id="site-header-cart" class="site-header-cart">
-			<li class="<?php echo esc_attr( $class ); ?>">
+			<li class="<?php echo esc_attr($class); ?>">
 				<?php _s_woocommerce_cart_link(); ?>
 			</li>
 			<li>
 				<?php
-					$instance = array(
-						'title' => '',
-					);
+                    $instance = array(
+                        'title' => '',
+                    );
 
-					the_widget( 'WC_Widget_Cart', $instance );
-				?>
+        the_widget('WC_Widget_Cart', $instance); ?>
 			</li>
 		</ul>
 		<?php
-	}
+    }
 }
