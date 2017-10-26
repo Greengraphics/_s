a695fc7ec664161073759712c06c2db83702e8b8
Nathan Gearing <natgearing@gmail.com>
2017-10-26 15:34:28 +1100

    the purge

--- functions.php
+++ functions.php
@@ -132,11 +132,6 @@ function ggstyle_scripts()
 add_action('wp_enqueue_scripts', 'ggstyle_scripts');
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
@@ -145,8 +140,3 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
